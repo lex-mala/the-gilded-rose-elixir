66afc13798b9ba39124a4ea808e1a519e3d4ca8d
Ernesto Malave <lex_nulla@protonmail.com>
2022-01-08 00:34:01 -0500

    kill the legacy updater

--- lib/gilded_rose/time.ex
+++ lib/gilded_rose/time.ex
@@ -20,6 +20,14 @@ defmodule GildedRose.Time do
     {:backstage, %Item{item | quality: 0, sell_in: sell_in - 1}}
   end
 
+  def pass({:basic, %Item{quality: quality, sell_in: sell_in} = item}) when quality < 2 do
+    {:basic, %Item{item | quality: 0, sell_in: sell_in - 1}}
+  end
+
+  def pass({:basic, %Item{quality: quality, sell_in: sell_in} = item}) do
+    {:basic, %Item{item | quality: quality - 1, sell_in: sell_in - 1}}
+  end
+
   def pass({:brie, %Item{quality: quality, sell_in: sell_in} = item}) when quality < 50 do
     {:brie, %Item{item | quality: quality + 1, sell_in: sell_in - 1}}
   end
@@ -42,115 +50,5 @@ defmodule GildedRose.Time do
 
   def pass({:sulfuras, %Item{} = item}) do
     {:sulfuras, item}
-  end
-
-  def pass({:basic, %Item{} = item}) do
-    item =
-      cond do
-        item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert" ->
-          if item.quality > 0 do
-            if item.name != "Sulfuras, Hand of Ragnaros" do
-              %{item | quality: item.quality - 1}
-            else
-              item
-            end
-          else
-            item
-          end
-
-        true ->
-          cond do
-            item.quality < 50 ->
-              item = %{item | quality: item.quality + 1}
-
-              cond do
-                item.name == "Backstage passes to a TAFKAL80ETC concert" ->
-                  item =
-                    cond do
-                      item.sell_in < 11 ->
-                        cond do
-                          item.quality < 50 ->
-                            %{item | quality: item.quality + 1}
-
-                          true ->
-                            item
-                        end
-
-                      true ->
-                        item
-                    end
-
-                  cond do
-                    item.sell_in < 6 ->
-                      cond do
-                        item.quality < 50 ->
-                          %{item | quality: item.quality + 1}
-
-                        true ->
-                          item
-                      end
-
-                    true ->
-                      item
-                  end
-
-                true ->
-                  item
-              end
-
-            true ->
-              item
-          end
-      end
-
-    item =
-      cond do
-        item.name != "Sulfuras, Hand of Ragnaros" ->
-          %{item | sell_in: item.sell_in - 1}
-
-        true ->
-          item
-      end
-
-    item =
-      cond do
-        item.sell_in < 0 ->
-          cond do
-            item.name != "Aged Brie" ->
-              cond do
-                item.name != "Backstage passes to a TAFKAL80ETC concert" ->
-                  cond do
-                    item.quality > 0 ->
-                      cond do
-                        item.name != "Sulfuras, Hand of Ragnaros" ->
-                          %{item | quality: item.quality - 1}
-
-                        true ->
-                          item
-                      end
-
-                    true ->
-                      item
-                  end
-
-                true ->
-                  %{item | quality: item.quality - item.quality}
-              end
-
-            true ->
-              cond do
-                item.quality < 50 ->
-                  %{item | quality: item.quality + 1}
-
-                true ->
-                  item
-              end
-          end
-
-        true ->
-          item
-      end
-
-    {:basic, item}
   end
 end
