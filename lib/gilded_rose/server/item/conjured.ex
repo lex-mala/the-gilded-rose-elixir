1d91fffd2be57b2df80ca43c958a57108e9450d5
Ernesto Malave <lex_nulla@protonmail.com>
2022-01-08 14:36:53 -0500

    move conjured impl back into main module

--- lib/gilded_rose/server/item/conjured.ex
+++ lib/gilded_rose/server/item/conjured.ex
@@ -1,25 +1,24 @@
 defmodule GildedRose.Server.Item.Conjured do
   defstruct [:item]
-end
 
-alias GildedRose.Server.Item.Conjured
+  defimpl GildedRose.Server.Item do
+    alias GildedRose.Utils
+    alias GildedRose.Server.Item.Conjured
 
-defimpl GildedRose.Server.Item, for: Conjured do
-  alias GildedRose.Utils
+    def age(%Conjured{item: %{quality: q} = item} = conjured) when q < 2 do
+      %Conjured{conjured | item: Utils.zero_quality(item)}
+    end
 
-  def age(%Conjured{item: %{quality: q} = item} = conjured) when q < 2 do
-    %Conjured{conjured | item: Utils.zero_quality(item)}
-  end
+    def age(%Conjured{item: %{sell_in: s} = item} = conjured) when s < 0 do
+      %Conjured{conjured | item: Utils.dec_quality(item, 4)}
+    end
 
-  def age(%Conjured{item: %{sell_in: s} = item} = conjured) when s < 0 do
-    %Conjured{conjured | item: Utils.dec_quality(item, 4)}
-  end
+    def age(%Conjured{item: item} = conjured) do
+      %Conjured{conjured | item: Utils.dec_quality(item, 2)}
+    end
 
-  def age(%Conjured{item: item} = conjured) do
-    %Conjured{conjured | item: Utils.dec_quality(item, 2)}
-  end
-
-  def mark_time(%Conjured{item: item} = conjured) do
-    %Conjured{conjured | item: Utils.dec_sell_in(item)}
+    def mark_time(%Conjured{item: item} = conjured) do
+      %Conjured{conjured | item: Utils.dec_sell_in(item)}
+    end
   end
 end
