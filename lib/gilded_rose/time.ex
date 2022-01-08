0f31a15e8de6e5b24255095a8e0b6cec9a978301
Ernesto Malave <lex_nulla@protonmail.com>
2022-01-08 13:06:31 -0500

    DRY it up a bit

--- lib/gilded_rose/time.ex
+++ lib/gilded_rose/time.ex
@@ -3,6 +3,51 @@ defmodule GildedRose.Time do
 
   @type item_type :: :backstage | :basic | :brie | :conjured | :sulfuras
 
+  @spec age({item_type(), %Item{}}) :: {item_type(), %Item{}}
+  def age({:backstage, %Item{sell_in: sell_in} = item}) when sell_in > 10 do
+    {:backstage, inc_quality(item, 1)}
+  end
+
+  def age({:backstage, %Item{sell_in: sell_in} = item}) when sell_in > 5 do
+    {:backstage, inc_quality(item, 2)}
+  end
+
+  def age({:backstage, %Item{sell_in: sell_in} = item}) when sell_in >= 0 do
+    {:backstage, inc_quality(item, 3)}
+  end
+
+  def age({:backstage, %Item{} = item}) do
+    {:backstage, zero_quality(item)}
+  end
+
+  def age({:basic, %Item{quality: quality} = item}) when quality < 2 do
+    {:basic, zero_quality(item)}
+  end
+
+  def age({:basic, %Item{} = item}) do
+    {:basic, dec_quality(item, 1)}
+  end
+
+  def age({:brie, %Item{quality: quality} = item}) when quality < 50 do
+    {:brie, inc_quality(item, 1)}
+  end
+
+  def age({:conjured, %Item{quality: quality} = item}) when quality < 2 do
+    {:conjured, zero_quality(item)}
+  end
+
+  def age({:conjured, %Item{sell_in: sell_in} = item}) when sell_in < 0 do
+    {:conjured, dec_quality(item, 4)}
+  end
+
+  def age({:conjured, %Item{} = item}) do
+    {:conjured, dec_quality(item, 2)}
+  end
+
+  def age({type, %Item{} = item}) do
+    {type, item}
+  end
+
   @spec tick({item_type(), %Item{}}) :: {item_type(), %Item{}}
   def tick({:sulfuras, %Item{} = item}) do
     {:sulfuras, item}
@@ -12,48 +57,15 @@ defmodule GildedRose.Time do
     {type, %Item{item | sell_in: sell_in - 1}}
   end
 
-  @spec weather({item_type(), %Item{}}) :: {item_type(), %Item{}}
-  def weather({:backstage, %Item{quality: quality, sell_in: sell_in} = item}) when sell_in > 10 do
-    {:backstage, %Item{item | quality: quality + 1}}
+  defp inc_quality(%Item{quality: quality} = item, value) do
+    %Item{item | quality: quality + value}
   end
 
-  def weather({:backstage, %Item{quality: quality, sell_in: sell_in} = item}) when sell_in > 5 do
-    {:backstage, %Item{item | quality: quality + 2}}
+  defp dec_quality(%Item{quality: quality} = item, value) do
+    %Item{item | quality: quality - value}
   end
 
-  def weather({:backstage, %Item{quality: quality, sell_in: sell_in} = item}) when sell_in >= 0 do
-    {:backstage, %Item{item | quality: quality + 3}}
-  end
-
-  def weather({:backstage, %Item{} = item}) do
-    {:backstage, %Item{item | quality: 0}}
-  end
-
-  def weather({:basic, %Item{quality: quality} = item}) when quality < 2 do
-    {:basic, %Item{item | quality: 0}}
-  end
-
-  def weather({:basic, %Item{quality: quality} = item}) do
-    {:basic, %Item{item | quality: quality - 1}}
-  end
-
-  def weather({:brie, %Item{quality: quality} = item}) when quality < 50 do
-    {:brie, %Item{item | quality: quality + 1}}
-  end
-
-  def weather({:conjured, %Item{quality: quality} = item}) when quality < 2 do
-    {:conjured, %Item{item | quality: 0}}
-  end
-
-  def weather({:conjured, %Item{quality: quality, sell_in: sell_in} = item}) when sell_in < 0 do
-    {:conjured, %Item{item | quality: quality - 4}}
-  end
-
-  def weather({:conjured, %Item{quality: quality} = item}) do
-    {:conjured, %Item{item | quality: quality - 2}}
-  end
-
-  def weather({type, %Item{} = item}) do
-    {type, item}
+  defp zero_quality(%Item{} = item) do
+    %Item{item | quality: 0}
   end
 end
